ece29a992b294aa99fbe22945f8b3d0861b4fe21
pengcheng <pengcheng3211@gmail.com>
2018-06-03 22:16:58 +0800

    增加getQuerySql无参数方法

--- src/main/java/com/sql/utils/SqlTemplatesUtils.java
+++ src/main/java/com/sql/utils/SqlTemplatesUtils.java
@@ -2,6 +2,7 @@ package com.sql.utils;
 
 import com.sql.extra.ApplicationContextHelper;
 import com.sql.extra.FreemarkerSqlTemplates;
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -26,6 +27,20 @@ public class SqlTemplatesUtils {
             Class classProperty, String methodName, Map<String, Object> params) {
         FreemarkerSqlTemplates messageSource = ApplicationContextHelper.getBean("freemarkerSqlTemplates");
         String sql = messageSource.process(classProperty.getSimpleName(), methodName, params);
+        return sql;
+    }
+
+    /**
+     * 获取查询sql，不传入查询条件。<br>
+     * @author pengc
+     * @param classProperty 类属性
+     * @param methodName 方法名称
+     * @return 组装好的查询sql
+     * @since 2018/6/1
+     */
+    public static String getQuerySql(Class classProperty, String methodName) {
+        FreemarkerSqlTemplates messageSource = ApplicationContextHelper.getBean("freemarkerSqlTemplates");
+        String sql = messageSource.process(classProperty.getSimpleName(), methodName, new HashMap<>());
         return sql;
     }
 
